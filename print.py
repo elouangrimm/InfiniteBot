d1155d735201b14c3d27dd26074ad23843a4f775
Elouan Grimm <elouan.grimm@gmail.com>
2024-03-13 17:36:28 -0700

    sorry delete

--- print.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-import time
-from infinitecraft import InfiniteCraft
-import infinitecraft
-
-async def main():
-    async with InfiniteCraft() as game:
-        print(game.discoveries)
-asyncio.run(main())
