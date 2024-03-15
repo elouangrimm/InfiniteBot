a6d6aa2ca7d53c6741d6fac8f98bf32b6f7ced70
Elouan Grimm <elouan.grimm@gmail.com>
2024-03-15 10:32:40 -0700

    Update individual.py

--- individual.py
+++ individual.py
@@ -3,25 +3,23 @@ import time
 from infinitecraft import InfiniteCraft
 import infinitecraft
 
+
 async def main():
-    async with InfiniteCraft() as game:
-        inputelement = input("Element: ")
-        while True:
-            for i in range(len(game.discoveries) - 1):
-                await asyncio.sleep(0.2)
-                if i < len(game.discoveries) - 1:
-                    print(
-                        f"Pairing elements: {game.discoveries[i]} and " + inputelement
-                    )
-                    element = infinitecraft.Element()
-                    element.name = inputelement
-                    result = await game.pair(game.discoveries[i],element)
-                    print(f"Result: {result}")
-                    print(f"Total: {len(game.discoveries)}")
-                    file = open("discoveries.txt", "a")
-                    file.write(
-                        f"\n{game.discoveries[i]}+" + inputelement + f"={result}"
-                    )
+  async with InfiniteCraft() as game:
+    inputelement = input("Element: ")
+    while True:
+      for i in range(len(game.discoveries) - 1):
+        await asyncio.sleep(0.2)
+        if i < len(game.discoveries) - 1:
+          print(f"Pairing {game.discoveries[i]} and " + inputelement)
+          element = infinitecraft.Element()
+          element.name = inputelement
+          result = await game.pair(game.discoveries[i], element)
+          print(f"Result: {result}")
+          print(f"Total: {len(game.discoveries)}")
+          file = open("discoveries.txt", "a")
+          file.write(f"\n{game.discoveries[i]}+" + inputelement + f"={result}")
+
 
 if __name__ == "__main__":
-    asyncio.run(main())
+  asyncio.run(main())
