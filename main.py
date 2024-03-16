0e2d76f084a9fe500e9b386ecfe78148adafaf22
Elouan Grimm <elouan.grimm@gmail.com>
2024-03-16 13:47:52 -0700

    UI

--- main.py
+++ main.py
@@ -2,77 +2,96 @@ import asyncio
 import craft
 import custom
 import individual
+import total
+from termcolor import colored
 
-print("""------------------------ INFINITEBOT ------------------------
+print(colored("""
+------------------------ INFINITEBOT ------------------------
 Created by Carson Bates and Elouan Grimm
 
-""")
-option = input("""Pick an option: 
-1: Random Run (Recomended)
-2: Individual (Run the program on one item)
-3: Custom (Run one custom pair)
-
-Option (1, 2, 3, Quit): """)
-
-if option == "1":
+""", "red"))
+while True:
   try:
-    print("""
+    option = input(colored("""Main Menu: 
+    1: Random Run (Recomended)
+    2: Individual (Run the program on one item)
+    3: Custom (Run one custom pair)
+    4: View Total Elements
+
+Option (1, 2, 3, 4, Quit): """, "white"))
+
+    if option == "1" or option == "":
+      try:
+        print(colored("""
 
 ------------------------ Starting Random Run ------------------------
-            """)
-    asyncio.run(craft.main())
-  except asyncio.TimeoutError as ex:
-    print("There was a timeout error:" + str(ex) +
-          "This is normal. The file should run again.")
-    asyncio.run(craft.main())
-  except KeyboardInterrupt:
-    print("""
-
-    ------------------------ Quiting ------------------------
-              """)
-    exit()
-
-if option == "2":
-  try:
-    print("""
-
-  ------------------------ Starting Individual ------------------------
-            """)
-    asyncio.run(individual.main())
-  except asyncio.TimeoutError:
-    print("There was a timeout error. This is normal. The file should run again.")
-    asyncio.run(craft.main())
-  except KeyboardInterrupt:
-    print("""
-
-    ------------------------ Quiting ------------------------
-              """)
-    exit()
-  except Exception as ex:
-    print("There was an error: " + str(ex) + " Please try to run the file again.")
-    exit()
-
-if option == "3":
-  try:
-    print("""
-
-  ------------------------ Starting Custom ------------------------
-            """)
-    asyncio.run(custom.main())
-  except asyncio.TimeoutError as ex:
-    print("There was a timeout error:" + str(ex) +
-          "This is normal. The file should run again.")
-    asyncio.run(craft.main())
-  except KeyboardInterrupt:
-    print("""
-
-    ------------------------ Quiting ------------------------
-              """)
-    exit()
-
-if option == "Quit":
-  print("""
+                """, "light_red"))
+        asyncio.run(craft.main())
+      except asyncio.TimeoutError as ex:
+        print("There was a timeout error:" + str(ex) +
+              "This is normal. The file should run again.")
+        asyncio.run(craft.main())
+      except KeyboardInterrupt:
+        print("""
 
 ------------------------ Quiting ------------------------
-          """)
-  exit()
+                  """)
+        exit()
+
+    if option == "2":
+      try:
+        print(colored("""
+
+------------------------ Starting Individual ------------------------
+                """, "light_red"))
+        asyncio.run(individual.main())
+      except asyncio.TimeoutError:
+        print("There was a timeout error. This is normal. The file should run again.")
+        asyncio.run(individual.main())
+      except KeyboardInterrupt:
+        print("""
+
+------------------------ Quiting ------------------------
+                  """)
+        exit()
+      except Exception as ex:
+        print("There was an error: " + str(ex) + " Please try to run the file again.")
+        exit()
+
+    if option == "3":
+      try:
+        print(colored("""
+
+------------------------ Starting Custom ------------------------
+                """, "light_red"))
+        asyncio.run(custom.main())
+      except asyncio.TimeoutError as ex:
+        print("There was a timeout error:" + str(ex) +
+              "This is normal. The file should run again.")
+        asyncio.run(custom.main())
+      except KeyboardInterrupt:
+        print("""
+
+------------------------ Quiting ------------------------
+                  """)
+        exit()
+
+    if option == "4":
+          print(colored("""
+
+
+------------------------------------------------ """, "light_red"))
+          asyncio.run(total.main())
+          print("""
+
+  """)
+          
+    if option == "Quit":
+        print("""
+
+------------------------ Quiting ------------------------
+              """)
+        exit()
+  except KeyboardInterrupt:
+    print("\n")
+    exit()
--- total.py
+++ total.py
@@ -7,4 +7,5 @@ async def main():
     async with InfiniteCraft() as game:
         print(f"Total: {len(game.discoveries)}.")
 
-asyncio.run(main()) 
+if __name__ == "__main__":
+    asyncio.run(main())
